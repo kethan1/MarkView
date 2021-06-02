3b9717f7bda68436406e9ee78015190d7a4423b4
Kethan Vegunta <kethan@vegunta.com>
2021-06-01 21:26:38 -0700

    make tests cross platform

--- tests/spectron_testing.test.js
+++ tests/spectron_testing.test.js
@@ -1,12 +1,30 @@
 const { Application } = require("spectron"),
     path = require("path"),
-    process = require("process");
+    process = require("process"),
+    os = require("os");
+
+var current_platform = os.platform();
+var current_arch = os.arch();
+
+var extension;
+switch (current_platform) {
+    case "win32":
+        extension = ".exe";
+        break;
+    case "darwin":
+        extension = ".app";
+        break;
+    default:
+        extension = "";
+}
+
+var path_to_app = `out/MarkView-${current_platform}-${current_arch === "x86" ? "ia32": current_arch}/markview${extension}${current_platform === "darwin" ? "/Contents/MacOS/markview": ""}`;
 
 const app = new Application({
     path: path.join(
         process.cwd(), // This works assuming you run npm test from project root
         // The path to the binary depends on your platform and architecture
-        `out/MarkView-win32-x64/markview.exe`
+        path_to_app
     )
 });
 
