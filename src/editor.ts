73102b808dec68724133c186d1289a38a302b974
Kethan Vegunta <kethan@vegunta.com>
2021-08-06 21:24:21 -0700

    be more consistent in single and double quotes

--- src/editor.ts
+++ src/editor.ts
@@ -10,27 +10,27 @@ const fs = require('fs'),
 
 /* eslint-disable */
 // @ts-ignore
-const savePath = path.join(ipcRenderer.sendSync('getPath', 'documents'), "MarkView/")
+const savePath = path.join(ipcRenderer.sendSync('getPath', 'documents'), 'MarkView/')
 
 if (!fs.existsSync(savePath)) {
     fs.mkdirSync(savePath);
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
     var easymde = new EasyMDE({ 
-        element: document.getElementById("editor"),
+        element: document.getElementById('editor'),
         forceSync: true,
         codeSyntaxHighlighting: true,
         indentWithTabs: false,
         tabSize: 4
     });
 
-    easymde.value(fs.readFileSync(path.join(savePath, sessionStorage.getItem("editing")), "utf8"))
+    easymde.value(fs.readFileSync(path.join(savePath, sessionStorage.getItem('editing')), 'utf8'))
 
     // easymde doesn't have a way to listen for changes, but listening for keypress should work just as well
-    document.querySelector(".EasyMDEContainer").addEventListener("keypress", function(key) { 
+    document.querySelector('.EasyMDEContainer').addEventListener('keypress', function(key) { 
         fs.writeFile(
-            path.join(savePath, sessionStorage.getItem("editing")),
+            path.join(savePath, sessionStorage.getItem('editing')),
             easymde.value(), 'utf8', function(err: Error) { if (err) throw err; }
         )
     });
--- src/home.ts
+++ src/home.ts
@@ -1,14 +1,14 @@
 /* eslint-disable */
 // @ts-ignore
-const fs = require("fs"), path = require("path"), { ipcRenderer }  = require('electron');
+const fs = require('fs'), path = require('path'), { ipcRenderer }  = require('electron');
 
 /* eslint-disable */
 // @ts-ignore
-const savePath = path.join(ipcRenderer.sendSync('getPath', 'documents'), "MarkView/"),
-    notes = document.getElementById("Notes");
+const savePath = path.join(ipcRenderer.sendSync('getPath', 'documents'), 'MarkView/'),
+    notes = document.getElementById('Notes');
 
-function openEditor(data: string, page = "editor.html"): void {
-    sessionStorage.setItem("editing", data)
+function openEditor(data: string, page = 'editor.html'): void {
+    sessionStorage.setItem('editing', data)
     window.location.href = page
 }
 
@@ -18,7 +18,7 @@ fs.readdir(savePath, (err: Error, files: string[]) => {
             input: fs.createReadStream(path.join(savePath, file)),
         });
         let lineCounter = 0,
-            lines = ""; 
+            lines = ''; 
         for await (const line of lineReader) {
             lineCounter++;
             if (lineCounter <= 5) lines += `${line}<br>`;
@@ -29,9 +29,9 @@ fs.readdir(savePath, (err: Error, files: string[]) => {
         else lines = lines.substring(0, lines.length - 4);
         
         notes.innerHTML += `
-            <div class="card grey lighten-4" style="width: fit-content; height: fit-content; cursor: pointer;" onclick="openEditor('${file}')">
-                <div class="card-content black-text">
-                    <span class="card-title">File: ${file}</span>
+            <div class='card grey lighten-4' style='width: fit-content; height: fit-content; cursor: pointer;' onclick='openEditor('${file}')'>
+                <div class='card-content black-text'>
+                    <span class='card-title'>File: ${file}</span>
                     <p>
                         Preview:
                         <br>
